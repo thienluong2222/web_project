b9f974815c05fc63dd22df074653e1151bb0d828
Thien <thienb2308392@student.ctu.edu.vn>
2025-07-14 20:48:58 +0700

    tu dong them san pham

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const submitButton = document.getElementById("login-button");
@@ -11,26 +10,48 @@ if (localStorage.getItem("isLoginedIn") === "true") {
 }
 
 //Lấy dữ liệu từ data.json
-const dataPath = '../src/data.json'
+const dataPath = "../src/data.json";
 fetch(dataPath)
-    .then(function(response) {
+    .then(function (response) {
         return response.json();
     })
-    .then(function(products) {
-        let htmls = products.map(function(product) {
-            return `<div class="main__div--produce">
-        //   <img src="${product.products.image_url}" alt="img" class="div__img--produce">
-          <div class="div__produce-item--info">
-            <p class="item--id">Mã Sản  Phẩm : ${product.products.id}</p>
-            <h3 class="item--name">${product.products.name}</h3>
-            <hr>
-            <h2>  ${product.products.price}</h2>
-          </div>
-          <a class="div__add-cart" href="#">Xem Chi Tiết</a>
-        </div>`;
-        });
-        var html = htmls.join('');
-        document.getElementById('Len').innerHTML = html;
+    .then(function (data) {
+        const products = data.products;
+
+        function createProductHtml(product) {
+            const formattedPrice = product.price.toLocaleString('vi-VN') + ' VNĐ';
+            const formattedOriginalPrice = product.original_price.toLocaleString('vi-VN') + ' VNĐ';
+
+            return `
+            <div class="main__div--produce">
+                <img src="${product.image_url}" alt="${product.name}" class="div__img--produce">
+                <div class="div__produce-item--info">
+                    <p class="item--id">Mã Sản Phẩm : ${product.id}</p>
+                    <h3 class="item--name">${product.name}</h3>
+                    <hr>
+                    <h2>${formattedPrice}</h2>
+                    ${product.original_price && product.original_price > product.price ? 
+                        `<p class="item--original-price">Giá gốc: <s>${formattedOriginalPrice}</s></p>` : ''}
+                </div>
+                <a class="div__add-cart" href="#">Xem Chi Tiết</a>
+            </div>`;
+        }
+
+        const len_catalogs = products
+            .filter(product => product.category_id === "Len")
+            .map(createProductHtml);
+
+        const thoiTrang_catalogs = products
+            .filter(product => product.category_id === "Thoi_Trang")
+            .map(createProductHtml);
+
+        const phuKien_catalogs = products
+            .filter(product => product.category_id === "Phu_Kien")
+            .map(createProductHtml);
+
+        document.getElementById("Len_catalog").innerHTML = len_catalogs.join("");
+        document.getElementById("ThoiTrang_catalog").innerHTML = thoiTrang_catalogs.join("");
+        document.getElementById("PhuKien_catalog").innerHTML = phuKien_catalogs.join("");
     });
 
 (function ($) {
@@ -133,5 +154,3 @@ fetch(dataPath)
         button.parent().parent().find("input").val(newVal);
     });
 })(jQuery);
-
-
