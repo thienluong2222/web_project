f8b06d5768cd0d437ad3abbdca5c072e3c60f7ac
trammphan <tramb2303785@student.ctu.edu.vn>
2025-07-18 11:53:46 +0700

    tram

--- public/js/product-items.js
+++ public/js/product-items.js
@@ -134,13 +134,8 @@ async function loadProductDetail() {
                 <div class="product-quantity-btn minus">−</div>
                 <div class="product-quantity-value" id="quantity">0</div>
                 <div class="product-quantity-btn plus">+</div>
-              </div>
-              <div>
-                <label class="product-quantity-label">Phân Loại</label>
-                <select name="type" id="id-product"></select>
-              </div>
+              </div>              
             </div>
-            <div></div>
             <div class="product-add-to-cart">
               <button class="product-add-to-cart-btn">
                 <i class="fas fa-shopping-cart"></i> Thêm vào giỏ hàng
